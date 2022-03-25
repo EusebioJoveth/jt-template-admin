3e74c5ee2ce5b2b69cf0a69bdc9fb55da00d06bb
Eusebio.joveth <eusebio.joveth@porlande.com>
2022-03-25 11:29:11 +0100

    estrutura da pasta

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="topo">
+        <span className="logo">JovethTecnologia</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+          <li>Dashboard</li>
+        </ul>
+      </div>
+      <div className="bottom">opções de cores</div>
+    </div>
+  )
+}
+
+export default Sidebar

1a6437728329b6730aec74e858defbeb5f36d785
Eusebio Joveth <eusebio.joveth@porlande.com>
2022-04-19 17:09:26 +0100

    New pag update

---
+++ src/formSource.js
@@ -0,0 +1,78 @@
+export const userInputs = [
+    {
+        id:1,
+        label: "Nome",
+        type: "text",
+        placeholder: "Eusébio",
+    },
+    {
+        id: 2,
+        label: "Sobrenome",
+        type: "text",
+        placeholder: "Joveth",
+    },
+    {
+        id: 3,
+        label: "Email",
+        type: "email",
+        placeholder: "eusebio.joveth@gmail.com",
+    },
+    {
+        id: 4,
+        label: "Telefone nº",
+        type: "text",
+        placeholder: "+244 935171658",
+    },
+    {
+        id: 5,
+        label: "Palavra Pass",
+        type: "password",
+    
+    },
+    {
+        id: 6,
+        label: "Endereço",
+        type: "text",
+        placeholder: "Zango 3 Viana. Luanda",
+    },
+    {
+        id: 7,
+        label: "País",
+        type: "text",
+        placeholder: "Angola",
+    },
+
+];
+
+export const productInputs = [
+    {
+        id: 1,
+        label: "Nome",
+        type: "text",
+        placeholder: "PC Aple Macbook Pro",
+    },
+    {
+        id: 2,
+        label: "Código",
+        type: "text",
+        placeholder: "Codigo do produto",
+    },
+    {
+        id: 3,
+        label: "Descrição",
+        type: "text",
+        placeholder: "Descrição",
+    },
+    {
+        id: 4,
+        label: "Categoria",
+        type: "text",
+        placeholder: "Computador",
+    },
+    {
+        id: 5,
+        label: "Stock",
+        type: "text",
+        placeholder: "Em stock",
+    }
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
-import './new.scss'
+import React from 'react';
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import { DriveFolderUploadOutlined } from '@mui/icons-material';
 
 const New = () => {
   return (
-    <div>New</div>
-  )
-}
+    <div className='new'>
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Adicionar Novo Utilizador</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+          </div>
+          <div className="right">
+            <form>
+            <div className="formInput">
+                <label htmlFor="file">
+                  Imagem: <DriveFolderUploadOutlined className='icon'/>
+                </label>
+                <input type="file" id='file' style={{display: "none"}}/>
+            </div>
+              <div className="formInput">
+                <label htmlFor="name">Nome</label>
+                <input type="text" placeholder='Eusebio' />
+              </div>
+              <div className="formInput">
+                <label htmlFor="lastname">Sobrenome</label>
+                <input type="text" placeholder='Joveth' />
+              </div>
+              <div className="formInput">
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder='eusebio@gmail.com' />
+              </div>
+              <div className="formInput">
+                <label htmlFor="phone">Telefone nº</label>
+                <input type="text" placeholder='+244 935171658' />
+              </div>
+              <div className="formInput">
+                <label htmlFor="password">Palavra Pass</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="address">Endereço</label>
+                <input type="text" placeholder='Zango 3 Viana. Luanda' />
+              </div>
+              <div className="formInput">
+                <label htmlFor="country">País</label>
+                <input type="text" placeholder='Angola' />
+              </div>
+              <button>Salvar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default New
